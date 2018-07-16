3fd9fd8151961ed9cb22877849113ab5b0491e1d
Chris Johnson <cc452@users.noreply.github.com>
2018-07-16 16:26:22 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+# Temporary install script.
+
+update_fedora () {
+	echo "-- Updating Fedora --"
+	echo ""
+	dnf -y update
+	echo ""
+}
+
+replace_firewalld () {
+	echo "-- Replacing Firewalld w/IPTables --"
+	echo ""
+	dnf -y install iptables-services
+	systemctl stop firewalld
+	systemctl enable iptables --now
+	dnf -y remove firewalld
+	iptables -F
+	echo ""
+}
+
+firewall_rules () {
+	iptables -I INPUT 2 -p tcp --dport 8000 -m comment --comment "SimEngine WebSocket" -j ACCEPT
+	iptables -I INPUT 2 -p udp --dport 8000 -m comment --comment "SimEngine WebSocket" -j ACCEPT
+	iptables -I INPUT 2 -p tcp --dport 9000 -m comment --comment "SimEngine FrontEnd" -j ACCEPT
+	iptables -I INPUT 2 -p udp --dport 9000 -m comment --comment "SimEngine FrontEnd" -j ACCEPT
+	iptables-save
+}
+
+install_utilities () {
+	echo "-- Installing Common Utilities --"
+	echo ""
+	dnf -y install nmap vim
+	echo ""
+}
+
+install_git () {
+	echo "-- Installing git --"
+	echo ""
+	dnf -y install git
+	echo ""
+}
+
+installl_nodejs () {
+	echo "-- Installing NodeJS --"
+	echo ""
+	dnf -y install nodejs
+	echo ""
+}
+
+set_hostname () {
+	echo "-- Setting Hostname --"
+	echo ""
+	hostnamectl set-hostname --static simengine
+	hostnamectl set-hostname --transient simengine
+	echo ""
+}
+
+clone_repo () {
+	echo "-- Cloning SimEngine Repo Into Home --"
+	echo ""
+#	git clone https://github.com/Seneca-CDOT/simengine.git
+	mkdir -p /usr/share/simengine/
+	chmod 755 -R /usr/share/simengine/
+	git clone https://github.com/cc452/simengine.git /usr/share/simengine/
+	echo ""
+}
+
+neo4j_repoadd () {
+	rpm --import http://debian.neo4j.org/neotechnology.gpg.key
+	echo "[neo4j]" > /etc/yum.repos.d/neo4j.repo
+	echo "name=Neo4j RPM Repository" >> /etc/yum.repos.d/neo4j.repo
+	echo "baseurl=http://yum.neo4j.org/stable" >> /etc/yum.repos.d/neo4j.repo
+	echo "enabled=1" >> /etc/yum.repos.d/neo4j.repo
+	echo "gpgcheck=1" >> /etc/yum.repos.d/neo4j.repo
+}
+
+database_install () {
+	dnf -y install redis neo4j python3-libvirt
+	python3 -m pip install -r /usr/share/simengine/enginecore/requirements.txt
+	pip install redis
+	pip install snmpsim
+}
+
+start_db () {
+	systemctl enable neo4j --now
+	systemctl enable redis --now
+}
+
+create_dbuser () {
+	sleep 4
+	echo "CALL dbms.changePassword('neo4j-simengine'); CALL dbms.security.createUser('simengine', 'simengine', false);"|cypher-shell -u neo4j -p neo4j
+	systemctl restart neo4j
+}
+
+install_coredaemon () {
+	cp /usr/share/simengine/services/simengine-core.service /etc/systemd/system/simengine-core.service
+	systemctl daemon-reload
+	systemctl enable simengine-core --now
+}
+
+npm_installation () {
+	echo "-- Running NPM Installation --"
+	echo ""
+	cd /usr/share/simengine/dashboard/frontend/
+	npm install
+	echo ""
+}
+
+npm_start () {
+	echo "-- Starting NodeJS --"
+	echo ""
+	cd /usr/share/simengine/dashboard/frontend/
+	npm start
+	echo ""
+}
+
+update_fedora
+replace_firewalld
+install_utilities
+install_git
+installl_nodejs
+set_hostname
+clone_repo
+neo4j_repoadd
+database_install
+start_db
+create_dbuser
+install_coredaemon
+npm_installation
+firewall_rules
+npm_start
